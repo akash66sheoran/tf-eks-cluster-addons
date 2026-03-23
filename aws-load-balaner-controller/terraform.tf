8952f94e72d117ab04b8af669bb2ae368f3ee09a
Akash <56023730+akash66sheoran@users.noreply.github.com>
2026-03-23 13:04:25 +0530

    added tokin field in kubernetes provider in terraform.tf

--- aws-load-balaner-controller/terraform.tf
+++ aws-load-balaner-controller/terraform.tf
@@ -22,18 +22,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-
-    command = "aws"
-
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      var.cluster_name
-    ]
-  }
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 provider "helm" {
