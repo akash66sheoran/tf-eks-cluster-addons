a7615e74a6eba536a6a6716f94fee4a8ca7122a0
akash66sheoran <akash66sheoran@gmail.com>
2026-03-27 14:18:33 +0530

    added terraform code for installing argocd

---
+++ argocd/main.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "9.4.16"
+
+  set = [
+    {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+  ]
+}
